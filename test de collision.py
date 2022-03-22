717f3e8c91fe28986dc7e70be4cee31db67ff809
RABE Marcellin <marcellinp20.aps1a@gmail.com>
2022-03-22 11:57:09 +0300

    Update test de collision.py

--- test de collision.py
+++ test de collision.py
@@ -1,6 +1,6 @@
-Test de collision [Backend physique]:
-on a un objet A à la position x0 et y0 et objet B à la postion x1 et y1.
+# Test de collision [Backend physique]:
+# on a un objet A à la position x0 et y0 et objet B à la postion x1 et y1.
 
-les deux objets sont carré et ont une surface de 2 (distance entre x = 1 et x = 3 par exemple).
+# les deux objets sont carré et ont une surface de 2 (distance entre x = 1 et x = 3 par exemple).
 
-écrire une fonction qui définie si deux objets sont entrée en collision dans le langage de votre choix.
+# écrire une fonction qui définie si deux objets sont entrée en collision dans le langage de votre choix.
